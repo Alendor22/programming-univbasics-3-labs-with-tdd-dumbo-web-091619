76daa22dabfbb78359351c4906087f7a5e0828d9
Adeja Lendor <alendor6@gmail.com>
2019-08-26 17:07:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
   first_number = 8
   second_number = 12
   sum = first_number + second_number
@@ -5,3 +6,13 @@
   product = first_number * second_number
   quotient = first_number / second_number
   
+=======
+def calculator(first_number, second_number)
+  first_number = Integer || Float
+  second_number = Integer || Float
+  second_number != 0
+  sum = first_number + second_number
+  print sum
+end
+calculator(3, 2)
+>>>>>>> 0c2d857661b6e807249e17da0ad6294f7347c43b
